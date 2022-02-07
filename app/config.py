6f7af7f2ba07b28108d9d71283625a55dcee484a
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-02-07 22:57:53 +0300

    removed config

--- app/config.py
+++ app/config.py
@@ -2,9 +2,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Access@localhost/quotes'
     SECRET_KEY = 'testsecretekey'
 
-
-
-
 class ProdConfig(Config):
     pass
 
