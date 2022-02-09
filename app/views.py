7f2adcd3bff85c14dbc1a744c3fa318dc61c57b7
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-02-09 06:58:19 +0300

    removedd previous

--- app/views.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from flask import render_template, redirect, request, url_for
-
-@app.route('/')
-def index():
-    title = "Sign-In page"
-    return render_template('login.html', title = title)
